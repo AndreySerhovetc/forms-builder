ee80bec5498d955efa3ed8c2ffaebb45b401c38e
Andrii Serhovetc <serhovetc88@gmail.com>
2021-10-04 19:14:38 +0300

    form-builder ver 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InfoElementComponent } from './info-element/info-element.component';
+import { DropFieldComponent } from './drop-field/drop-field.component';
+import { ListOfElementComponent } from './list-of-element/list-of-element.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoElementComponent,
+    DropFieldComponent,
+    ListOfElementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
